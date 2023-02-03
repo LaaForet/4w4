95c16e22b66161e157457a38ee8b395ee110c23a
Clara Champagne-Laforest <96385675+LaaForet@users.noreply.github.com>
2023-02-03 09:29:48 -0500

    s2c2 divise footer en 3 colonnes

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
-<footer>
-    <h3>Ceci est le pied de page</h3>
+<footer class="site__footer">
+    <!-- <h3>Ceci est le pied de page</h3> -->
+    <section>Colonne 1</section>
+    <section>Colonne 2</section>
+    <section>Colonne 3</section>
 </footer>
 <?php wp_footer(); ?>
