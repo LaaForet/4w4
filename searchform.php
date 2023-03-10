f6dac0a9d8ef5c4eaddf98970271e1f2b22a0502
Clara Champagne-Laforest <96385675+LaaForet@users.noreply.github.com>
2023-03-09 19:12:32 -0500

    styles barre de recherche

--- searchform.php
+++ searchform.php
@@ -4,8 +4,9 @@
 <form class="recherche" role="search" method="get"  action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <label>
     <input class="recherche__input" type="search" class="search-field" placeholder="rechercher..." value="<?php echo get_search_query(); ?>" name="s" />
+    <button class="recherche__bouton" type="submit" class="search-submit">
+      <span class="recherche__icone"><i class="fa-solid fa-magnifying-glass"></i></span>
+    </button>  
   </label>
-  <button class="recherche__bouton" type="submit" class="search-submit">
-    <span class="recherche__icone">&#x1F50D;</span>
-  </button>
+  
 </form>
