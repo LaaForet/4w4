64ef4b4d1bfe463719d9fb2dd85cdc1931fc7a56
Clara Champagne-Laforest <96385675+LaaForet@users.noreply.github.com>
2023-03-15 17:45:23 -0400

    mise en page footer

--- footer.php
+++ footer.php
@@ -1,7 +1,22 @@
 <footer class="site__footer">
     <!-- <h3>Ceci est le pied de page</h3> -->
-    <section>Colonne 1</section>
-    <section>Colonne 2</section>
-    <section>Colonne 3</section>
+    <section class="site__footer__logo"><?php the_custom_logo() ?></section>
+    <section class="site__footer__nav">
+        <h2>Liens rapides</h2>
+        <?php 
+            wp_nav_menu(array(
+                'menu' => 'entete', 
+                'container' => 'nav'
+                ))
+        ?>
+    </section>
+    <section class="site__footer__medias">
+        <h2>Suivez-nous !</h2>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-github"></i>
+        <i class="fa-brands fa-linkedin"></i>
+    </section>
+
 </footer>
 <?php wp_footer(); ?>
