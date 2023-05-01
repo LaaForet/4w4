6a2a115f744f2a06c0235ce32084dded2bbabbf2
Clara Champagne-Laforest <96385675+LaaForet@users.noreply.github.com>
2023-05-01 14:37:53 -0400

    ajout description pages

--- functions.php
+++ functions.php
@@ -46,6 +46,26 @@
     }
     add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
 
+    /**
+     * Pour ajouter la description des événements dans le menu événement
+     */
+    function ajouter_description_class_menu( $items, $args ) {
+        // Vérifier si le menu correspondant est celui que vous souhaitez modifier
+        if ( 'evenement' === $args->menu ) {
+            foreach ( $items as $item ) {
+                // Récupérer le titre, la description et la classe personnalisée
+                $titre = $item->title;
+                $description = $item->description;
+                //$classe = $item->class; // Remplacer par le nom de la classe souhaitée
+    
+                // Ajouter la description et la classe personnalisée à l'élément de menu
+                $item->title .= '<span>' . $description . '</span>';
+            }
+        }
+        return $items;
+    }
+    add_filter( 'wp_nav_menu_objects', 'ajouter_description_class_menu', 10, 2 );
+
     // add_theme_support()
     add_theme_support(  'title-tag');
     add_theme_support(  'custom-logo', array(
